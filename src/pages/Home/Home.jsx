7d6a3febcfe250be3f34b210f786576fe6c6cd2e
Jijesh <jijeshjiju0272@gmail.com>
2024-05-24 11:20:33 +0530

    Initial

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Welcome from '../Welcome/Welcome'
+import Booking from '../Booking/Booking'
+import Dishes from '../Dishes/Dishes'
+import Ambience from '../Ambience/Ambience'
+import Delivery from '../Delivery/Delivery'
+
+function Home() {
+  return (
+<>
+
+<Welcome/>
+<Booking/>
+<Dishes/>
+<Ambience/>
+<Delivery/>
+
+</>
+  )
+}
+
+export default Home
